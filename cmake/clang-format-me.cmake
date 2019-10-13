6a3c7537f0480bf6354cd502a51cd2adc8b8a64f
Wiktor Drewniak <wiktor.drewniak@gmail.com>
2019-10-13 22:18:05 +0200

    Clang format 1st release

--- cmake/clang-format-me.cmake
+++ cmake/clang-format-me.cmake
@@ -1,5 +1,5 @@
 
-
+set(CLANG_FORMAT_EXE clang-format-6.0)
 
 function(clang_format_me target_name)
     # set(oneValueArgs DESTINATION RENAME)
@@ -10,10 +10,9 @@ function(clang_format_me target_name)
     # exclude pattern
 
     add_custom_target(clang-format-all)
-    add_custom_target(clang-format-${target_name})
-    add_dependencies(clang-format-all clang-format-${target_name})
-
     get_target_property(target_sources_list ${target_name} SOURCES)
+    list(TRANSFORM target_sources_list PREPEND "${CMAKE_CURRENT_SOURCE_DIR}/")
+
     get_target_property(target_include_directories_list ${target_name} INCLUDE_DIRECTORIES)
 
     foreach(current_dir ${target_include_directories_list})
@@ -23,20 +22,22 @@ function(clang_format_me target_name)
                 *.[h]pp
         )
         foreach(found_header ${found_target_header_list})
-            list(APPEND target_sources_list ${current_dir}/${found_header})
+            list(APPEND target_sources_list "${current_dir}/${found_header}")
         endforeach()
     endforeach()
 
-    foreach(file ${${target_sources_list}})
-        message(${file})
-        string(REPLACE "/" "_" CURRENT_TARGET ${file})
-        add_custom_command(OUTPUT ${file}.clang-format
-                            COMMAND echo ${file}
-                            COMMAND touch ${file}.clang-format
-                            BYPRODUCTS ${file}.clang-format
+    foreach(file ${target_sources_list})
+        string(REPLACE ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR} output_file ${file})
+        get_filename_component(output_path ${output_file} DIRECTORY)
+
+	    add_custom_command(OUTPUT ${output_file}.clang_format
+                            COMMAND ${CLANG_FORMAT_EXE} -i ${file}
+                            COMMAND mkdir -p ${output_path}
+                            COMMAND touch ${output_file}.clang_format
                             DEPENDS ${file})
-        add_custom_target(${CURRENT_TARGET}.clang-format-marker
-                          DEPENDS ${file}.clang-format)
-        add_dependencies(clang-format-${target_name} ${CURRENT_TARGET}.clang-format-marker)
+        list(APPEND target_stamps ${output_file}.clang_format)
     endforeach()
+
+    add_custom_target(clang-format-${target_name} DEPENDS ${target_stamps})
+    add_dependencies(clang-format-all clang-format-${target_name})
 endfunction()
